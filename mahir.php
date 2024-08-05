0b371aed314d8687affe7d0bed2e4907299118c1
Dev-Rehman-Ali <rehamanli.zakirhussain@akshayakalpa.org>
2024-08-05 16:42:06 +0530

    my code

--- mahir.php
+++ mahir.php
@@ -1,4 +1,21 @@
 <?php    
 
 echo "hello mahirhusen gfhgfhgfg";
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "Hello Git hub";
+        exit;
+    ?>
+</body>
+</html>
